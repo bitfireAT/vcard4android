ee8e390dc4ec4459f76dcb722ec55c1f1389da40
Ricki Hirner <hirner@bitfire.at>
2015-09-18 14:17:22 +0200

    Initial commit

---
+++ src/androidTest/java/at/bitfire/vcard4android/ApplicationTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright © 2013 – 2015 Ricki Hirner (bitfire web engineering).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ */
+
+package at.bitfire.vcard4android;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+	public ApplicationTest() {
+		super(Application.class);
+	}
+}
