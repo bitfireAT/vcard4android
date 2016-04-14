046cdbf76911b2979c19e9219b069386d4b4cba7
Ricki Hirner <hirner@bitfire.at>
2016-04-14 20:07:15 +0200

    Tests
    * gradle: grant Android 6 runtime permissions for tests
    * use lists instead of deprecated @Getters

--- src/androidTest/java/at/bitfire/vcard4android/ContactTest.java
+++ src/androidTest/java/at/bitfire/vcard4android/ContactTest.java
@@ -10,21 +10,16 @@ package at.bitfire.vcard4android;
 
 import android.content.res.AssetManager;
 import android.test.InstrumentationTestCase;
-import android.util.Log;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Locale;
 
-import ezvcard.VCard;
 import ezvcard.VCardVersion;
-import ezvcard.io.text.VCardWriter;
 import ezvcard.parameter.AddressType;
 import ezvcard.parameter.EmailType;
 import ezvcard.parameter.ImppType;
@@ -69,25 +64,25 @@ public class ContactTest extends InstrumentationTestCase {
         assertEquals("Laastnehm", c.phoneticFamilyName);
 
         // TEL
-        assertEquals(2, c.getPhoneNumbers().size());
-        Telephone phone = c.getPhoneNumbers().get(0);
+        assertEquals(2, c.phoneNumbers.size());
+        Telephone phone = c.phoneNumbers.get(0);
         assertTrue(phone.getTypes().contains(TelephoneType.VOICE));
         assertTrue(phone.getTypes().contains(TelephoneType.HOME));
         assertTrue(phone.getTypes().contains(TelephoneType.PREF));
         assertNull(phone.getPref());
         assertEquals("+49 1234 56788", phone.getText());
-        phone = c.getPhoneNumbers().get(1);
+        phone = c.phoneNumbers.get(1);
         assertTrue(phone.getTypes().contains(TelephoneType.FAX));
         assertEquals("+1-800-MYFAX", phone.getText());
 
         // EMAIL
-        assertEquals(2, c.getEmails().size());
-        Email email = c.getEmails().get(0);
+        assertEquals(2, c.emails.size());
+        Email email = c.emails.get(0);
         assertTrue(email.getTypes().contains(EmailType.HOME));
         assertTrue(email.getTypes().contains(EmailType.PREF));
         assertNull(email.getPref());
         assertEquals("private@example.com", email.getValue());
-        email = c.getEmails().get(1);
+        email = c.emails.get(1);
         assertTrue(email.getTypes().contains(EmailType.WORK));
         assertEquals("work@example.com", email.getValue());
 
@@ -100,19 +95,19 @@ public class ContactTest extends InstrumentationTestCase {
         assertEquals("Programmer", c.jobDescription);
 
         // IMPP
-        assertEquals(3, c.getImpps().size());
-        Impp impp = c.getImpps().get(0);
+        assertEquals(3, c.impps.size());
+        Impp impp = c.impps.get(0);
         assertTrue(impp.getTypes().contains(ImppType.PERSONAL));
         assertTrue(impp.getTypes().contains(ImppType.MOBILE));
         assertTrue(impp.getTypes().contains(ImppType.PREF));
         assertNull(impp.getPref());
         assertEquals("myIM", impp.getProtocol());
         assertEquals("anonymous@example.com", impp.getHandle());
-        impp = c.getImpps().get(1);
+        impp = c.impps.get(1);
         assertTrue(impp.getTypes().contains(ImppType.BUSINESS));
         assertEquals("skype", impp.getProtocol());
         assertEquals("echo@example.com", impp.getHandle());
-        impp = c.getImpps().get(2);
+        impp = c.impps.get(2);
         assertEquals("sip", impp.getProtocol());
         assertEquals("mysip@example.com", impp.getHandle());
 
@@ -120,8 +115,8 @@ public class ContactTest extends InstrumentationTestCase {
         assertTrue(Arrays.equals(new String[] { "Nick1", "Nick2" }, c.nickName.getValues().toArray()));
 
         // ADR
-        assertEquals(2, c.getAddresses().size());
-        Address addr = c.getAddresses().get(0);
+        assertEquals(2, c.addresses.size());
+        Address addr = c.addresses.get(0);
         assertTrue(addr.getTypes().contains(AddressType.WORK));
         assertTrue(addr.getTypes().contains(AddressType.POSTAL));
         assertTrue(addr.getTypes().contains(AddressType.PARCEL));
@@ -134,7 +129,7 @@ public class ContactTest extends InstrumentationTestCase {
         assertEquals("NC", addr.getRegion());
         assertEquals("27613-3502", addr.getPostalCode());
         assertEquals("U.S.A.", addr.getCountry());
-        addr = c.getAddresses().get(1);
+        addr = c.addresses.get(1);
         assertTrue(addr.getTypes().contains(AddressType.WORK));
         assertEquals("Postfach 314", addr.getPoBox());
         assertEquals("vorne hinten", addr.getExtendedAddress());
@@ -150,13 +145,13 @@ public class ContactTest extends InstrumentationTestCase {
         // CATEGORIES
         assertTrue(Arrays.equals(
                 new String[]{"A", "B'C"},
-                c.getCategories().toArray()
+                c.categories.toArray()
         ));
 
         // URL
-        assertEquals(2, c.getURLs().size());
+        assertEquals(2, c.urls.size());
         boolean url1 = false, url2 = false;
-        for (Url url : c.getURLs()) {
+        for (Url url : c.urls) {
             if ("https://davdroid.bitfire.at/".equals(url.getValue()) && url.getType() == null)
                 url1 = true;
             if ("http://www.swbyps.restaurant.french/~chezchic.html".equals(url.getValue()) && "x-blog".equals(url.getType()))
@@ -171,12 +166,12 @@ public class ContactTest extends InstrumentationTestCase {
         assertEquals("2014-08-12", dateFormat.format(c.anniversary.getDate()));
 
         // RELATED
-        assertEquals(2, c.getRelations().size());
-        Related rel = c.getRelations().get(0);
+        assertEquals(2, c.relations.size());
+        Related rel = c.relations.get(0);
         assertTrue(rel.getTypes().contains(RelatedType.CO_WORKER));
         assertTrue(rel.getTypes().contains(RelatedType.CRUSH));
         assertEquals("Ägidius", rel.getText());
-        rel = c.getRelations().get(1);
+        rel = c.relations.get(1);
         assertTrue(rel.getTypes().contains(RelatedType.PARENT));
         assertEquals("muuuum", rel.getText());
 
@@ -189,19 +184,19 @@ public class ContactTest extends InstrumentationTestCase {
         Contact c = regenerate(parseContact("allfields-vcard3.vcf", null), VCardVersion.V4_0);
         // let's check only things that should be different when VCard 4.0 is generated
 
-        Telephone phone = c.getPhoneNumbers().get(0);
+        Telephone phone = c.phoneNumbers.get(0);
         assertFalse(phone.getTypes().contains(TelephoneType.PREF));
         assertNotNull(phone.getPref());
 
-        Email email = c.getEmails().get(0);
+        Email email = c.emails.get(0);
         assertFalse(email.getTypes().contains(EmailType.PREF));
         assertNotNull(email.getPref());
 
-        Impp impp = c.getImpps().get(0);
+        Impp impp = c.impps.get(0);
         assertFalse(impp.getTypes().contains(ImppType.PREF));
         assertNotNull(impp.getPref());
 
-        Address addr = c.getAddresses().get(0);
+        Address addr = c.addresses.get(0);
         assertFalse(addr.getTypes().contains(AddressType.PREF));
         assertNotNull(addr.getPref());
     }
