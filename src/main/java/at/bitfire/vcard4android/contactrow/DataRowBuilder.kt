67e96730c58b37ad76981a5efcba30427caf8719
Sunik Kupfer <kupfer@bitfire.at>
2022-10-19 12:47:06 +0200

    add KDoc params to DataRowBuilder class

--- src/main/java/at/bitfire/vcard4android/contactrow/DataRowBuilder.kt
+++ src/main/java/at/bitfire/vcard4android/contactrow/DataRowBuilder.kt
@@ -12,10 +12,14 @@ import at.bitfire.vcard4android.Contact
 /**
  * Builder for a data row to insert into the Android contact provider.
  *
+ * @param mimeType      the type of data in this row, see [ContactsContract.CommonDataKinds]
  * @param dataRowUri    data row URI, including callerIsSyncAdapter=… if required
  * @param rawContactId  not *null*: raw contact ID to assign this data row to;
  *                      *null*: data row will be assigned to back reference with index 0
  *                      (= result ID of the first operation in batch, which must be the insert operation to insert the raw contact)
+ * @param contact       which contact this data row belongs to
+ * @param readOnly      whether the data row is read-only (write protected). When the address book is
+ *                      read-only, all contacts inside are too, and so the contact's data rows should be as well.
  */
 abstract class DataRowBuilder(
     val mimeType: String,
